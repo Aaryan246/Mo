4fe8bdf90d39783bde936cc0c4a00b8dee5f98b6
Aaryan <aaryan24602@gmail.com>
2022-10-28 22:05:41 +0530

    added route

--- mo/src/App.js
+++ mo/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+import Motive from "./pages/Motive"
+import Motivation from "./pages/Motivation"
+import Movement from "./pages/Movement"
+import Profile from "./pages/Profile"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Login/> } />
+        <Route path="signup" element={ <Signup/> } />
+        <Route path="home" element={ <Home/> } />
+        <Route path="motive" element={ <Motive/> } />
+        <Route path="movement" element={ <Movement/> } />
+        <Route path="motivation" element={ <Motivation/> } />
+        <Route path="profile" element={ <Profile/> } />
+      </Routes>
     </div>
   );
 }
---
+++ mo/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+  
+export default Home;
+  
